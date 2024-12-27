8a85ac1ab2b0f1fac4999c76d4cdf9bb0509bdbe
NathanTh3Gr3at <112683403+NathanTh3Gr3at@users.noreply.github.com>
2024-12-27 12:18:18 +0200

    Refactor navigation structure and implement meal details screen with favorite toggle functionality. Completed first part of flutter meal app section

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/meal_details.dart';
 import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
     super.key,
-    required this.title,
-    required this.meals,
+    this.title,
+    required this.meals,required this.onToggleFavorite,
   });
+  final void Function(Meal meal) onToggleFavorite;
+  void selectMeal(
+    BuildContext context,
+    Meal meal,
+  ) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(
+          meal: meal,
+          onToggleFavorite: onToggleFavorite,
+        ),
+      ),
+    );
+  }
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
 
   @override
@@ -38,13 +53,21 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (ctx, index) => MealItem(meal:meals[index])
+        itemBuilder: (ctx, index) => MealItem(
+          meal: meals[index],
+          onSelectMeal: (meal) {
+            selectMeal(context, meal);
+          },
+        ),
       );
+    }
+    if(title==null){
+      return content;
     }
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -4,7 +4,7 @@ import 'package:meals_app/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key, required this.meal});
+  const MealItem({super.key, required this.meal, required this.onSelectMeal});
   final Meal meal;
   String get complexityText{
     return meal.complexity.name[0].toUpperCase()+meal.complexity.name.substring(1);
@@ -12,6 +12,7 @@ class MealItem extends StatelessWidget {
   String get affordabilityText{
     return meal.affordability.name[0].toUpperCase()+meal.affordability.name.substring(1);
   }
+  final void Function(Meal meal) onSelectMeal;
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +24,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
