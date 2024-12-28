ccfc537b8352065772a1df709087b6b0b14098fd
NathanTh3Gr3at <112683403+NathanTh3Gr3at@users.noreply.github.com>
2024-12-28 10:08:21 +0200

    Refactor CategoriesScreen to StatefulWidget, add animation for category selection, and implement Hero widget for meal images in MealDetailsScreen and MealItem. Completed the Meals app from flutter tutorial

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -16,35 +16,49 @@ class MealDetailsScreen extends ConsumerWidget {
     final favoriteMeals = ref.watch(favoriteMealsProvider);
 
     final isFavorite = favoriteMeals.contains(meal);
-print('isFavorite: $isFavorite');
+
     return Scaffold(
-      
         appBar: AppBar(title: Text(meal.title), actions: [
           IconButton(
             onPressed: () {
               final wasAdded = ref
                   .read(favoriteMealsProvider.notifier)
                   .toggleMealFavoriteStatus(meal);
-                  print('wasAdded: $wasAdded');
+
               ScaffoldMessenger.of(context).clearSnackBars();
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
                   content: Text(
-                      isFavorite? 'Meal added as a favorite.' : 'Meal removed.'),
+                    wasAdded ? 'Meal added as a favorite.' : 'Meal removed.',
+                  ),
                 ),
               );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                    turns: Tween(begin: 0.5, end: 1.0).animate(animation),
+                    child: child);
+              },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+            ),
           )
         ]),
         body: SingleChildScrollView(
           child: Column(
             children: [
-              Image.network(
-                meal.imageUrl,
-                height: 300,
-                width: double.infinity,
-                fit: BoxFit.cover,
+              Hero(
+                tag: meal.id,
+                child: Image.network(
+                  meal.imageUrl,
+                  height: 300,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
               ),
               const SizedBox(height: 14),
               Text(
